f5578ae50d4f4b85767d9f469bceaffde5136833
Juan Fonseca <juanma2268@gmail.com>
2019-08-02 17:35:01 -0600

    Incorporated shapefile to the Android app

--- app/src/main/java/open/juanf/shapefilevisualizer/MainActivity.java
+++ app/src/main/java/open/juanf/shapefilevisualizer/MainActivity.java
@@ -20,6 +20,7 @@
 package open.juanf.shapefilevisualizer;
 
 import android.app.AlertDialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -45,6 +46,10 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.content.res.Resources;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     private final static String TAG = MainActivity.class.getSimpleName();
@@ -62,7 +67,25 @@ public class MainActivity extends AppCompatActivity {
         ArcGISMap map = new ArcGISMap(Basemap.createStreetsVector());
         mMapView.setMap(map);
 
-        requestReadPermission();
+        requestReadWritePermission();
+    }
+
+    // https://stackoverflow.com/questions/8664468/copying-raw-file-into-sdcard
+    private void writeIntoExternalStorage(int resourceId, String somePathOnSdCard){
+        try {
+            InputStream in = getResources().openRawResource(resourceId);
+            FileOutputStream out = new FileOutputStream(somePathOnSdCard);
+            byte[] buff = new byte[1024];
+            int read = 0;
+            while ((read = in.read(buff)) > 0) {
+                out.write(buff, 0, read);
+            }
+            in.close();
+            out.close();
+            Log.d(TAG,String.format("Wrote '%s'",somePathOnSdCard));
+        } catch (Exception e){
+            Log.e(TAG,e.getMessage());
+        }
     }
 
     @Override
@@ -111,9 +134,16 @@ public class MainActivity extends AppCompatActivity {
      * Creates a ShapefileFeatureTable from a service and, on loading, creates a FeatureLayer and add it to the map.
      */
     private void featureLayerShapefile() {
+        String pathSDCard = Environment.getExternalStorageDirectory() + "/Download/"
+                + getString(R.string.shapefile_name);
+        writeIntoExternalStorage(R.raw.cantones_de_costa_rica_shp,String.format("%s.shp",pathSDCard));
+        writeIntoExternalStorage(R.raw.cantones_de_costa_rica_shx,String.format("%s.shx",pathSDCard));
+        writeIntoExternalStorage(R.raw.cantones_de_costa_rica_prj,String.format("%s.prj",pathSDCard));
+        writeIntoExternalStorage(R.raw.cantones_de_costa_rica_dbf,String.format("%s.dbf",pathSDCard));
+        writeIntoExternalStorage(R.raw.cantones_de_costa_rica_cpg,String.format("%s.cpg",pathSDCard));
+
         // load the shapefile with a local path
-        ShapefileFeatureTable shapefileFeatureTable = new ShapefileFeatureTable(
-                Environment.getExternalStorageDirectory() + getString(R.string.shapefile_path));
+        ShapefileFeatureTable shapefileFeatureTable = new ShapefileFeatureTable(String.format("%s.shp",pathSDCard));
 
         shapefileFeatureTable.loadAsync();
         shapefileFeatureTable.addDoneLoadingListener(() -> {
@@ -138,13 +168,18 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Request read permission on the device.
      */
-    private void requestReadPermission() {
+    private void requestReadWritePermission() {
         // define permission to request
-        String[] reqPermission = new String[] { Manifest.permission.READ_EXTERNAL_STORAGE };
-        int requestCode = 2;
+        String[] reqPermission = new String[] {
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE };
+        int requestCode = 2; // 2
         // For API level 23+ request permission at runtime
-        if (ContextCompat.checkSelfPermission(MainActivity.this,
-                reqPermission[0]) == PackageManager.PERMISSION_GRANTED) {
+
+        int permission1 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[0]);
+        int permission2 = ContextCompat.checkSelfPermission(MainActivity.this, reqPermission[1]);
+
+        if (permission1 == PackageManager.PERMISSION_GRANTED && permission2 == PackageManager.PERMISSION_GRANTED) {
             featureLayerShapefile();
         } else {
             // request permission
@@ -156,7 +191,8 @@ public class MainActivity extends AppCompatActivity {
      * Handle the permissions request response.
      */
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED
+            && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
             featureLayerShapefile();
         } else {
             // report to user that permission was denied
