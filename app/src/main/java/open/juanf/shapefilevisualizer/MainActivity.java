9412f7cd70cc73bbe730a631e2c79c5ab1218763
Juan Fonseca <juanma2268@gmail.com>
2019-08-10 17:05:04 -0600

    Modified license

--- app/src/main/java/open/juanf/shapefilevisualizer/MainActivity.java
+++ app/src/main/java/open/juanf/shapefilevisualizer/MainActivity.java
@@ -1,23 +1,41 @@
-/* Copyright 2019 Juan Fonseca-Solis (https://github.com/juanfonsecasolis/shapefileVisualizer)
- * Copyright 2018 ESRI
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * This file has been modified by Juan Fonseca-Solis to incorporate a menu bar, load the
- * shapefiles from the APK instead of manually copying them from console, and present a demo
- * of a shapefile containing data from Portal de datos abiertos: Cantones de Costa Rica
- * (http://daticos-geotec.opendata.arcgis.com/datasets/741bdd9fa2ca4d8fbf1c7fe945f8c916_0)
+/*  
+ * Copyright (c) 2019  Juan M. Fonseca-Solis   
+ *  
+ * This file is free software: you may copy, redistribute and/or modify it  
+ * under the terms of the GNU General Public License as published by the  
+ * Free Software Foundation, either version 2 of the License, or (at your  
+ * option) any later version.  
+ *  
+ * This file is distributed in the hope that it will be useful, but  
+ * WITHOUT ANY WARRANTY; without even the implied warranty of  
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU  
+ * General Public License for more details.  
+ *  
+ * You should have received a copy of the GNU General Public License  
+ * along with this program.  If not, see .  
  * 
+ * Changes: incorporated a menu bar and the functionality to load a shapefiles from
+ * the APK instead of manually copying them from console. The offered shapefile was 
+ * taken from Portal de datos abiertos: Cantones de Costa Rica
+ * (http://daticos-geotec.opendata.arcgis.com/datasets/741bdd9fa2ca4d8fbf1c7fe945f8c916_0)
+ *
+ * This file incorporates work covered by the following copyright and  
+ * permission notice: 
+ * 	
+ *	Copyright 2018 ESRI
+ *
+ *	Licensed under the Apache License, Version 2.0 (the "License");
+ *	you may not use this file except in compliance with the License.
+ *	You may obtain a copy of the License at
+ *
+ *		http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *	Unless required by applicable law or agreed to in writing, software
+ *	distributed under the License is distributed on an "AS IS" BASIS,
+ *	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *	See the License for the specific language governing permissions and
+ *	limitations under the License.
+ *
  */
 
 package open.juanf.shapefilevisualizer;
